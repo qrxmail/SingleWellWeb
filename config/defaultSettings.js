ab7f7fb0c881b9d5cdb58010b6b0cf782cd50f6d
qrxmail <qrxmail@126.com>
2020-09-14 15:11:36 +0800

    CityGasJS

---
+++ config/defaultSettings.js
@@ -0,0 +1,19 @@
+const proSettings = {
+  navTheme: 'dark',
+  //navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'top',
+  contentWidth: 'Fluid',
+  fixedHeader: true,
+  fixSiderbar: true,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: '城燃设备管理',
+  pwa: false,
+  iconfontUrl: '',
+  footerRender:false,
+};
+export default proSettings;
